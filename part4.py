bacbba0453dc4be4ca5b790fbd1e1101ec9e1b67
sd3765 <47250686+sd3765@users.noreply.github.com>
2021-12-13 02:05:11 -0500

    Create part4.py

---
+++ part4.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+fileName = "data.txt"
+blockSize = []
+performance = []
+file = open(fileName, mode = 'r')
+for line in file:
+    line = line.split()
+    blockSize.append(line[0])
+    performance.append(float(line[1]))
+print(blockSize,performance)
+x = blockSize
+y = performance
+plt.plot(x,y)
+plt.xlabel('blockSize')
+plt.ylabel('performance (MiB/s)')
+plt.title('Performance Vs. BlockSize')
+for i,j in zip(x,y):
+    plt.text(i,j,str(j))
+plt.savefig("PlotWithCache.png")
+file.close()
