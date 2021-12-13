bdcfc4349b904f82b4afaf447ba04c5a78a81912
sd3765 <47250686+sd3765@users.noreply.github.com>
2021-12-13 03:11:01 -0500

    Update part3.py

--- part3.py
+++ part3.py
@@ -10,9 +10,9 @@ for line in file:
     line2 = file.readline()
     line2 = line2.split()
     blockSizeWithoutCache.append(line1[0])
-    performanceWithoutCache.append(float(line1[1]))
+    performanceWithoutCache.append(round(float(line1[1]),1))
     blockSizeWithCache.append(line2[0])
-    performanceWithCache.append(float(line2[1]))
+    performanceWithCache.append(round(float(line2[1]),1))
     
 
 x1 = blockSizeWithoutCache
@@ -20,22 +20,13 @@ y1 = performanceWithoutCache
 x2 = blockSizeWithCache
 y2 = performanceWithCache
 
-print(x1, y1)
-plt.subplot(2,1,1)
+
 plt.plot(x1,y1)
 plt.xlabel('blockSize')
 plt.ylabel('performance (MiB/s)')
-plt.title('Performance Vs. BlockSize')
-for i,j in zip(x1,y1):
-    plt.text(i,j, str(j))
-
-plt.subplot(2,1,2)
+plt.title('Performance Vs. BlockSize Without Cache')
 plt.plot(x2,y2)
-plt.xlabel('blockSize')
-plt.ylabel('performance (MiB/s)')
-plt.title('Performance Vs. BlockSize')
-for i,j in zip(x2,y2):
-    plt.text(i,j, str(j))
+plt.legend(['Without Cache', 'With Cache'])
 plt.savefig("plot.png")
 
 file.close()
