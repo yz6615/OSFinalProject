9a1577ef3aa6f0fdc601fdbfb3686de293a9f793
sd3765 <47250686+sd3765@users.noreply.github.com>
2021-12-13 02:57:15 -0500

    Update part3.py

--- part3.py
+++ part3.py
@@ -1,20 +1,41 @@
 import matplotlib.pyplot as plt
 fileName = "data.txt"
-blockSize = []
-performance = []
+blockSizeWithoutCache = []
+performanceWithoutCache = []
+blockSizeWithCache = []
+performanceWithCache = []
 file = open(fileName, mode = 'r')
 for line in file:
-    line = line.split()
-    blockSize.append(line[0])
-    performance.append(float(line[1]))
-print(blockSize,performance)
-x = blockSize
-y = performance
-plt.plot(x,y)
+    line1 = line.split()
+    line2 = file.readline()
+    line2 = line2.split()
+    blockSizeWithoutCache.append(line1[0])
+    performanceWithoutCache.append(float(line1[1]))
+    blockSizeWithCache.append(line2[0])
+    performanceWithCache.append(line2[1])
+    
+
+x1 = blockSizeWithoutCache
+y1 = performanceWithoutCache
+x2 = blockSizeWithCache
+y2 = performanceWithCache
+
+print(x1, y1)
+plt.subplot(2,1,1)
+plt.plot(x1,y1)
 plt.xlabel('blockSize')
 plt.ylabel('performance (MiB/s)')
 plt.title('Performance Vs. BlockSize')
-for i,j in zip(x,y):
+for i,j in zip(x1,y1):
     plt.text(i,j, str(j))
-plt.savefig("plotWithoutCache.png")
+
+plt.subplot(2,1,2)
+plt.plot(x2,y2)
+plt.xlabel('blockSize')
+plt.ylabel('performance (MiB/s)')
+plt.title('Performance Vs. BlockSize')
+for i,j in zip(x2,y2):
+    plt.text(i,j, str(j))
+plt.savefig("plot.png")
+
 file.close()
